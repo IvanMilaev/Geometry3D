8483f21839dde44b2a071d794a5092629e6aa5cc
Ivan Milaev <milaevivan@gmail.com>
2019-05-18 20:17:25 +0300

    Initial Commit

---
+++ Geometry3D_02/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Geometry3D_02
+//
+//  Created by Иван Милаев on 18/05/2019.
+//  Copyright © 2019 IvanMilaev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
